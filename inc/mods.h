a833793462424a4f08f213bd764ff9356df635e1
Zleub <debray.arnaud@gmail.com>
2017-01-12 23:03:50 +0100

    typos

--- inc/mods.h
+++ inc/mods.h
@@ -6,7 +6,7 @@
 /*   By: adebray <adebray@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/03 15:44:58 by adebray           #+#    #+#             */
-/*   Updated: 2016/04/23 19:25:54 by adebray          ###   ########.fr       */
+/*   Updated: 2017/01/12 15:54:16 by adebray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,9 @@
 
 # include <stdlib.h>
 # include <stdio.h>
+
 # include <value.h>
+
 # include <table.h>
 # include <stack.h>
 
